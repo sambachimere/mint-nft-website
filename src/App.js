800acae70580aac995064397e2e7aa90b9fa6d3e
Samba Diaw <sambachimerediaw@gmail.com>
2022-02-05 01:20:08 +0100

    owner can withdraw money from contract

--- src/App.js
+++ src/App.js
@@ -61,8 +61,25 @@ function App() {
     }
   }
 
+  async function withdraw() {
+    if(typeof window.ethereum !== 'undefined') {
+      let accounts = await window.ethereum.request({method: 'eth_requestAccounts'});
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+      const contract = new ethers.Contract(GGaddress, GreedyGeese.abi, signer);
+      try {
+        const transaction = await contract.withdraw();
+        await transaction.wait();
+      }
+      catch(err) {
+        setError(err.message);
+      }
+    }
+  }
+
   return (
     <div className="App">
+      <button className="withdraw" onClick={withdraw}>Withdraw</button>
       <div className="container">
         <div className="banniere">
           <img src={img1} alt="img" />
